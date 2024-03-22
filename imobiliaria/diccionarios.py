f5ba41df95b0001df3834d11a12accb091a02cf6
Sabuat Urbina Ribeiro <90584688+sabuat@users.noreply.github.com>
2024-03-22 17:55:35 -0300

    Add files via upload
    Dicionários e funções da regra de negocios

---
+++ imobiliaria/diccionarios.py
@@ -0,0 +1,171 @@
+# Criando diccionarios
+
+imoveis = {
+    1 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO371238/02803A360CFA4298E039612C67102610.JPG',
+        'tipo' : 'Aluguel',
+        'cidade': 'Bragança Paulista',
+        'endereco': 'Rua das Flores, 123',
+        'descricao': 'Casa aconchegante com 2 quartos, 1 banheiro e quintal espaçoso.',
+        'valor': 2500
+    },
+    2 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO403354/7FBB672555C77008CF2AEE35C094681E.JPEG',
+        'tipo' : 'Aluguel',
+        'cidade': 'Campinas',
+        'endereco': 'Avenida Principal, 456',
+        'descricao': 'Casa térrea com 3 quartos, 2 banheiros e área de lazer com churrasqueira.',
+        'valor': 3000
+    },
+    3 :  {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO579434/A226CF21CBE9904027E35CDD63D2816B.JPEG',
+        'tipo' : 'Aluguel',
+        'cidade': 'Sorocaba',
+        'endereco': 'Rua da Amizade, 789',
+        'descricao': 'Casa com 4 quartos, 3 banheiros, piscina e ampla área verde.',
+        'valor': 5000
+    },
+    4 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO363971/6E0F11DACF51DD3C056043303A77047B.JPG',
+        'tipo' : 'Aluguel',
+        'cidade': 'Piracicaba',
+        'endereco': 'Rua dos Coqueiros, 101',
+        'descricao': 'Casa moderna com 2 suítes, sala de estar ampla e cozinha gourmet.',
+        'valor': 4500
+    },
+    5 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO8512/PV8224021.jpg',
+        'tipo' : 'Aluguel',
+        'cidade': 'Jundiaí',
+        'endereco': 'Travessa das Palmeiras, 222',
+        'descricao': 'Casa espaçosa com 5 quartos, 4 banheiros e área de serviço completa.',
+        'valor': 6000
+    },
+    6 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO479508/5F1CEAC54061CA414CD941662027135E.JPG',
+        'tipo' : 'Aluguel',
+        'cidade': 'Itu',
+        'endereco': 'Rua das Oliveiras, 333',
+        'descricao': 'Casa mobiliada com 3 quartos, 2 banheiros, piscina e jardim bem cuidado.',
+        'valor': 7000
+    },
+    7 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO321726/D36401CB7C3E846BC205EB857EF10BA3.JPG',
+        'tipo' : 'Aluguel',
+        'cidade': 'São José dos Campos',
+        'endereco': 'Avenida das Acácias, 444',
+        'descricao': 'Casa com vista panorâmica, 4 quartos, 3 banheiros e área de lazer completa.',
+        'valor': 8000
+    },
+    8 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO191010/369F4B805A6BD97F79B9AA0C4CF400BF.JPG',
+        'tipo' : 'Aluguel',
+        'cidade': 'São Roque',
+        'endereco': 'Rua dos Ipês, 555',
+        'descricao': 'Casa rústica com 2 quartos, 1 banheiro, lareira e área externa com pomar.',
+        'valor': 3500
+    },
+    9 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO903770/0C3D96F4EF3CB8E28F16F12CFCFB151F.JPG',
+        'tipo' : 'Venda',
+        'cidade': 'Limeira',
+        'endereco': 'Rua das Azaleias, 6685',
+        'descricao': 'Casa térrea com 3 quartos, 2 banheiros, garagem para 2 carros e jardim.',
+        'valor': 280000
+    },
+    10 : {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO869960/4070CEDBEDAB5C8E80ACE77515A86C95.JPG',
+        'tipo' : 'Venda',
+        'cidade': 'Indaiatuba',
+        'endereco': 'Rua das Violetas, 777',
+        'descricao': 'Casa com piscina, 4 quartos, 3 banheiros e área gourmet com churrasqueira.',
+        'valor': 550000
+    },
+    11: {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO192890/A6508AA2E577799CF3D3F7E33E2442E5.JPG',
+        'tipo' : 'Venda',
+        'cidade': 'Taubaté',
+        'endereco': 'Avenida das Palmeiras, 888',
+        'descricao': 'Casa com 3 suítes, sala de estar ampla, área de serviço e quintal.',
+        'valor': 400000
+    },
+    12: {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO374538/E55333285A2AE4332F1CBBF567BA409D.JPG',
+        'tipo' : 'Venda',
+        'cidade': 'Jacareí',
+        'endereco': 'Rua das Orquídeas, 999',
+        'descricao': 'Casa em condomínio fechado com 4 quartos, 3 banheiros e segurança 24 horas.',
+        'valor': 90000
+    },
+    13: {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO500842/85B384AF72272A1662FD2725398DA9A6.JPG',
+        'tipo' : 'Venda',
+        'cidade': 'Araras',
+        'endereco': 'Travessa dos Girassóis, 1234',
+        'descricao': 'Casa térrea com 2 quartos, 1 banheiro, área de serviço e quintal amplo.',
+        'valor': 230000
+    },
+    14: {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO323330/4EFC0E01E2911DC997737DE1AE4210E6.JPG',
+        'tipo' : 'Venda',
+        'cidade': 'Americana',
+        'endereco': 'Avenida dos Lírios, 4321',
+        'descricao': 'Casa com 3 quartos, 2 banheiros, garagem coberta e área de lazer com piscina.',
+        'valor': 320000
+    },
+    15: {
+        'imagem': 'https://betaimages.lopes.com.br/realestate/sml/REO192514/F092087F10B9F1577A5A46974B67157B.JPG',
+        'tipo' : 'Venda',
+        'cidade': 'Mogi Mirim',
+        'endereco': 'Rua dos Cravos, 5678',
+        'descricao': 'Casa térrea com 2 quartos, 1 banheiro e área externa com jardim.',
+        'valor': 210000
+    }    
+}
+
+
+def gerar_id():
+    x = len(imoveis) + 1
+    return x
+
+def crear_imovel():
+    tipo = int(input('Ingrese o tipo de Imovel: (1) Aluguel  (2) Venda'))
+    cidade = (input('Informe a cidade: '))
+    endereco = (input('Informe o endereço: '))
+    descricao = (input('Descrição do imovel: '))
+    valor = (input('Informe o valor: '))
+    if tipo == 1:
+        imoveis[gerar_id()] = {'tipo': 'Aluguel', 'cidade': cidade, 'endereco': endereco, 'descricao': descricao, 'valor': valor}
+    elif tipo == 2:
+        imoveis[gerar_id()] = {'tipo': 'Vendas', 'cidade': cidade, 'endereco': endereco, 'descricao': descricao, 'valor': valor}
+    else:
+        print('Este valor nao e valido')
+
+def filtrar_imoveis():
+    lista = int(input('Ingrese o tipo de Imovel que deseja visualizar: (1) Aluguel  (2) Venda (3)Todos'))
+    if lista == 1:
+        for im in imoveis.values():
+            for value in im.values():
+                if value == 'Aluguel':
+                    return im
+    elif lista == 2:
+        for im in imoveis.values():
+            for value in im.values():
+                if value == 'Venda':
+                    return im
+    elif lista == 3:
+        return imoveis
+    else:
+        print('Este valor nao e valido')
+
+def mostrar_imoveis():
+    return imoveis
+
+def mostrar_imovel(ident:int):
+    return imoveis[ident]
+
+def apagar_imoveis(id:int):
+    del imoveis[id]
+
+def modif_imoveis(id:int, dados_im: dict):
+    imoveis[id] = dados_im
